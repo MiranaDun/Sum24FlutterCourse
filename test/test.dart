2fa32cb7dbdcb073cef10fe44de592d5400e86da
Arina Zimina <a.zimina@innopolis.university>
2024-06-26 19:58:46 +0300

    test

--- test/test.dart
+++ test/test.dart
@@ -1,12 +1,12 @@
-
+import 'package:education/templates/lab7/main.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  group('Empty Validation', () {
-    test('Empty string returns true', () {
-      expect(true, true);
-    });
-  });
+  testWidgets('MyHomePage displays "Hello World!"', (WidgetTester tester) async {
+    await tester.pumpWidget(const ProviderScope(child: MaterialApp(home: MyHomePage())));
 
+    expect(find.text('Hello World'), findsOneWidget);
+  });
 }
